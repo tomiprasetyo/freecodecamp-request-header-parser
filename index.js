e71395ba1c77fcc96726aeaa94c7839c39a392cd
Tomi Prasetyo <tomi.prasetyo95@gmail.com>
2023-08-29 16:46:12 +0700

    fix request

--- index.js
+++ index.js
@@ -6,14 +6,6 @@ require("dotenv").config();
 var express = require("express");
 var app = express();
 
-app.get("/api/whoami", function (req, res) {
-  const ip = req.headers["x-forwarded-for"];
-  // console.log(JSON.stringify(req.headers));
-  const language = req.headers["accept-language"];
-  const software = req.headers["user-agent"];
-  res.send({ ip: ip, language: language, software: software });
-});
-
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require("cors");
@@ -30,6 +22,17 @@ app.get("/", function (req, res) {
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
+});
+
+app.get("/api/whoami", (req, res) => {
+  let yourIP = req.ip;
+  let yourLanguage = req.header("accept-language");
+  let yourSoftware = req.header("user-agent");
+  res.json({
+    ipaddress: yourIP,
+    language: yourLanguage,
+    software: yourSoftware,
+  });
 });
 
 // listen for requests :)
